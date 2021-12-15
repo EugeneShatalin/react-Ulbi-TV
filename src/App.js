a63ca7308d74b7439d8d1f138b33eba036f47606
EugeneShatalin <44637183+EugeneShatalin@users.noreply.github.com>
2021-12-15 12:41:45 +0600

    feat: create functional component

--- src/App.js
+++ src/App.js
@@ -1,30 +1,14 @@
 import React, { useState } from 'react';
+import Counter from './components/Counter';
 
-function App() {
-
-  const [likes, setLikes] = useState(5);
+function App() { 
   const [value, setValue] = useState('text');
-
-  function increment() {
-    setLikes(likes + 1)
-  }
-
-  function decrement() {
-    setLikes(likes - 1)
-  }
+  
   return (
     <div className="App">
-      <h1>{likes}</h1>
-      <h1>{value}</h1>
+      
+      <Counter/>
 
-      <input 
-      type="text" 
-      value={value}
-      onChange={event => setValue(event.target.value)}>        
-      </input>
-
-      <button onClick={increment}>increment</button>
-      <button onClick={decrement}>decrement</button>
     </div>
   );
 }
