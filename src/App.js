d0fe67e053e341a9bc9f5ea9093a59bc99daa5e6
EugeneShatalin <44637183+EugeneShatalin@users.noreply.github.com>
2021-12-15 13:42:16 +0600

    feat: create class component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import ClassCounter from './components/ClassCounter';
 import Counter from './components/Counter';
 
 function App() {
   return (
     <div className="App">
       
-      <Counter/>
+     <Counter/>
+
+      <ClassCounter/>
 
     </div>
   );
