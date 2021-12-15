710bb4bacff0c9eeb644973dda29ffb84a2377c4
EugeneShatalin <44637183+EugeneShatalin@users.noreply.github.com>
2021-12-16 00:11:17 +0600

    feat: create a PostList component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import React from 'react';
-import ClassCounter from './components/ClassCounter';
-import Counter from './components/Counter';
-import PostItem from './components/PostItem';
+import React, { useState} from 'react';
+import Postlist from './components/PostList';
 import './styles/App.css';
 
 
-function App() {
-  return (
+function App() {  
+  const [posts, setPosts] = useState([
+    { id: 1, title: 'Javascript', body: 'Description 1' },
+    { id: 2, title: 'HTML', body: 'Description 2' },
+    { id: 3, title: 'CSS', body: 'Description 3' },
+  ]);
+  const [posts2, setPosts2] = useState([
+    { id: 1, title: 'Python', body: 'Description 1' },
+    { id: 2, title: 'PHP', body: 'Description 2' },
+    { id: 3, title: 'Node.js', body: 'Description 3' },
+  ]);
+
+
+  return (    
     <div className="App">
-      <PostItem post={{id: 1, title: 'Javascript', body: 'Description'}}/>      
+      <Postlist posts={posts} title='Посты про Фронтенд'/>
+      <Postlist posts={posts2} title='Посты про Бэкенд' />
     </div>
   );
 }
