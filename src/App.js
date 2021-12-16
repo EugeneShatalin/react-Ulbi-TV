a8edacd444b258553f928c1f3ede1db3df4e2252
EugeneShatalin <44637183+EugeneShatalin@users.noreply.github.com>
2021-12-16 12:28:33 +0600

    feat: create component for Button

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState} from 'react';
 import Postlist from './components/PostList';
+import Mybutton from './components/UI/button/MyButton';
 import './styles/App.css';
 
 
@@ -8,18 +9,17 @@ function App() {
     { id: 1, title: 'Javascript', body: 'Description 1' },
     { id: 2, title: 'HTML', body: 'Description 2' },
     { id: 3, title: 'CSS', body: 'Description 3' },
-  ]);
-  const [posts2, setPosts2] = useState([
-    { id: 1, title: 'Python', body: 'Description 1' },
-    { id: 2, title: 'PHP', body: 'Description 2' },
-    { id: 3, title: 'Node.js', body: 'Description 3' },
-  ]);
+  ]); 
 
 
   return (    
     <div className="App">
-      <Postlist posts={posts} title='Посты про Фронтенд'/>
-      <Postlist posts={posts2} title='Посты про Бэкенд' />
+      <form>
+        <input type="text" placeholder="Название поста"/>
+        <input type="text" placeholder="Описание поста"/>
+        <Mybutton disabled>Создать пост</Mybutton>
+      </form>
+      <Postlist posts={posts} title='Посты про Фронтенд'/>    
     </div>
   );
 }
