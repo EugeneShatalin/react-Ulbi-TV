49fb2e602a229ae4ef2338d079917c8112b9b2fd
EugeneShatalin <44637183+EugeneShatalin@users.noreply.github.com>
2021-12-17 18:35:45 +0600

    feat: add the PostFilter component to improve the work of the Postlist component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import React, {useState} from 'react';
+import React, {useMemo, useState} from 'react';
 import Postlist from './components/PostList';
-import Myinput from './components/UI/input/MyInput';
 import './styles/App.css';
 import PostForm from "./components/PostForm";
-import MySelect from "./components/UI/select/MySelect";
+import PostFilter from "./components/PostFilter";
 
 
 function App() {
@@ -12,19 +11,21 @@ function App() {
     {id: 2, title: 'HTML', body: 'Description 2'},
     {id: 3, title: 'CSS', body: 'Description 3'},
   ]);
-  const [selectedSort, setSelectedSort] = useState('');
-  const [searchQuery, setSearchQuery] = useState('');
+  const [filter, setFilter] = useState({sort: '', query: ''});
 
-  const getSortedPosts = () => {
+
+  const sortedPosts = useMemo(() => {
     console.log("Отработала функция сортировки постов")
-    if (selectedSort) {
-      return [...posts].sort((a, b) => a[selectedSort].localeCompare(b[selectedSort]));
+    if (filter.sort) {
+      return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]));
     }
-
     return posts;
-  }
+  }, [filter.sort, posts]);
 
-  const sortedPost = getSortedPosts();
+  const sortAndSearchedPosts = useMemo(() => {
+    return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query.toLowerCase()));
+  }, [filter.query, sortedPosts])
+
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
@@ -34,38 +35,15 @@ function App() {
     setPosts(posts.filter(p => p.id !== post.id));
   }
 
-  const sortPosts = (sort) => {
-    setSelectedSort(sort);
-  }
-
-
   return (
     <div className="App">
       <PostForm create={createPost}/>
       <hr style={{margin: '15px 0'}}/>
-      <div>
-        <Myinput
-          value={searchQuery}
-          onChange={e => setSearchQuery(e.target.value)}
-          placeholder='Поиск...'
-        />
-        <MySelect
-          value={selectedSort}
-          onChange={sortPosts}
-          defaultValue='Сортировка'
-          options={[
-            {value: 'title', name: 'По названию'},
-            {value: 'body', name: 'По описанию'},
-          ]}
-        />
-      </div>
-      {posts.length
-        ?
-        <Postlist posts={sortedPost} remove={removePost} title='Посты про Фронтенд'/>
-        :
-        <h1 style={{textAlign: 'center'}}>Посты не найдены!</h1>
-      }
-
+      <PostFilter
+        filter={filter}
+        setFilter={setFilter}
+      />
+      <Postlist posts={sortAndSearchedPosts} remove={removePost} title='Посты про Фронтенд'/>
     </div>
   );
 }
